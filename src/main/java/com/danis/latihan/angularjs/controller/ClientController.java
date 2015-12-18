9edfab62d75585570b0a5fb00f2c5be3e6227ab9
a.supriatna <a.supriatna@voxteneo.asia>
2015-12-18 13:40:52 +0700

    create feature api crud

--- src/main/java/com/danis/latihan/angularjs/controller/ClientController.java
+++ src/main/java/com/danis/latihan/angularjs/controller/ClientController.java
@@ -7,12 +7,19 @@ package com.danis.latihan.angularjs.controller;
 
 import com.danis.latihan.angularjs.dao.ClientDao;
 import com.danis.latihan.angularjs.entity.Client;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -28,11 +35,41 @@ public class ClientController {
     
     @RequestMapping(value = "/client", method = RequestMethod.GET)
     public Page<Client> getFindClient(Pageable page){
-        return cd.findAll(page);
+        Page result = cd.findAll(page);
+        System.out.println("Total Jumlah Data: "+result.getTotalElements());
+        return result;
     }
     
     @RequestMapping(value = "/client", method = RequestMethod.POST)
-    public void clientSave(@RequestBody Client c){
-        cd.save(c);
+    public ResponseEntity<Client> clientSave(@RequestBody Client c){
+        Client result = cd.save(c);
+        if (result == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
+    
+    @RequestMapping(value = "/client", method = RequestMethod.PUT)
+    public  ResponseEntity<Client> clientUpdate(@RequestBody Client c){
+        Client result = cd.save(c);
+        if (result == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+        }
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+   
+    @RequestMapping(value = "/client/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Client> clientFindbyId(@PathVariable String id){
+        Client result = cd.findOne(id);
+        if (result == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return  new ResponseEntity<>(result, HttpStatus.OK);
+    }
+    
+    @RequestMapping(value = "/client/{id}", method = RequestMethod.DELETE)
+    public @ResponseBody ResponseEntity<Void> clientDelete(@PathVariable String id){
+        cd.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
